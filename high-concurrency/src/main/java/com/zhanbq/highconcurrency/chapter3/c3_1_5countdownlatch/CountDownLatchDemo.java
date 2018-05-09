68d5f64f4b82a05d533f269b9132b53a8e1b299f
zhanbqlocal <996388047@qq.com>
2018-05-09 21:48:02 +0800

    倒计时器

---
+++ high-concurrency/src/main/java/com/zhanbq/highconcurrency/chapter3/c3_1_5countdownlatch/CountDownLatchDemo.java
@@ -0,0 +1,40 @@
+package com.zhanbq.highconcurrency.chapter3.c3_1_5countdownlatch;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CountDownLatchDemo implements Runnable {
+  
+  static final CountDownLatch end = new CountDownLatch(10);
+  
+  static final CountDownLatchDemo demo = new CountDownLatchDemo();
+  
+  @Override
+  public void run() {
+    try {
+      Thread.sleep(new Random().nextInt(10)*1000);
+      System.out.println("check complete");
+      end.countDown();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
+  }
+  
+  public static void main(String[] args) throws InterruptedException {
+    ExecutorService exec = Executors.newFixedThreadPool(10);
+    for (int i = 0; i < 10; i++) {
+      exec.submit(demo);
+    }
+    
+    end.await(); //等待检查任务全部结束
+    
+    System.out.println("检查完毕,发射");
+    
+    exec.shutdown();
+    
+  }
+  
+}
